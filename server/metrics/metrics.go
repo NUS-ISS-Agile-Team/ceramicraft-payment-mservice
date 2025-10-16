98ef421149a2dee9f4a4358e0bd50a94a8cf0998
YangZhicheng <yangzhicheng@YangZhichengdeMacBook-Air.local>
2025-10-16 19:01:37 +0800

    chore: change metrics's name

--- server/metrics/metrics.go
+++ server/metrics/metrics.go
@@ -6,8 +6,8 @@ var (
 	// 请求总数（用于吞吐量、QPS）
 	HttpRequestsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "payment_service_http_requests_total",
-			Help: "Total number of HTTP requests processed.(支付服务请求总量)",
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests processed.",
 		},
 		[]string{"method", "path"},
 	)
@@ -15,8 +15,8 @@ var (
 	// 请求耗时（用于P95、P99）
 	HttpRequestDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "payment_service_http_request_duration_milliseconds",
-			Help:    "Histogram of response latency (seconds) of HTTP requests.(支付服务响应耗时ms)",
+			Name:    "http_request_duration_milliseconds",
+			Help:    "Histogram of response latency (seconds) of HTTP requests.",
 			Buckets: []float64{5, 10, 25, 50, 100, 250, 500, 1000, 2000, 5000}, // 5ms~5s
 		},
 		[]string{"method", "path"},
@@ -25,7 +25,7 @@ var (
 	// 错误总数
 	HttpRequestsErrors = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "payment_service_errors_total.(支付服务错误总数)",
+			Name: "http_errors_total.",
 			Help: "Total number of error responses (status >= 400).",
 		},
 		[]string{"method", "path", "status"},
